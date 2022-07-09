71a39b27079195bc807c27be4df7ea097303d280
Yasuhiro Usutani <y_usutani@mac.com>
2022-07-09 14:13:43 +0900

    3: 同一メールアドレスの登録防止

--- app/controllers/bulk_accounts_controller.rb
+++ app/controllers/bulk_accounts_controller.rb
@@ -4,10 +4,20 @@ class BulkAccountsController < ApplicationController
   end
 
   def create
+    is_success = false
     @accounts = accounts_params.map { Account.new(_1).tap(&:valid?) }
-    if @accounts.none? { _1.errors.any? }
-      @accounts.each(&:save)
-      redirect_to new_bulk_account_url
+    ActiveRecord::Base.transaction do
+      is_success = @accounts.none? { _1.errors.any? } and @accounts.all?(&:save!)
+      raise ActiveRecord::RecordInvalid unless is_success
+    end
+  rescue ActiveRecord::RecordInvalid
+    is_success = false
+  rescue ActiveRecord::RecordNotUnique
+    is_success = false
+    flash.now[:alert] = "Account already exists."
+  ensure
+    if is_success
+      redirect_to new_bulk_account_url, notice: "Accounts were successfully created."
     else
       render :new, status: :unprocessable_entity
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_09_050301) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_09_051108) do
   create_table "accounts", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_accounts_on_email", unique: true
   end
 
 end
